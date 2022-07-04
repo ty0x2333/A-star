061a3e9c8b5396ae8565da7313ad06b0a3490630
ty0x2333 <luckytianyiyan@gmail.com>
2022-07-04 09:15:45 +0800

    Update archive

---
+++ src/settings.524a4.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scene/main.fire",orientation:"landscape",jsList:[],bundleVers:{internal:"d0832",main:"60ff1"}};
